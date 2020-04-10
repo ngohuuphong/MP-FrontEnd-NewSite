8c3d118abdbc6d348c36a4513f1ff5ecbdbb9e30
Ngo Huu Phong <ngohuuphong.147@gmail.com>
2020-04-10 09:58:55 +0900

    Create page

--- layout/leftside_bar.php
+++ layout/leftside_bar.php
@@ -2,16 +2,16 @@
     <nav class="left-nav">
         <ul id="nav">
             <li class="current">
-                <a href="#welcome"> <?= includeSVG("arrow-circle-up") ?>トップ</a>
+                <a href="?page=page10"> <?= includeSVG("arrow-circle-up") ?>トップ</a>
             </li>
-            <li><a href="#"> <?= includeSVG("user-cog") ?> 学生検索</a></li>
-            <li><a href="#"> <?= includeSVG("email") ?> メッセージ <i class="counter"> 14 </i></a></li>
-            <li><a href="#"> <?= includeSVG("table") ?>スケジュール管理</a></li>
-            <li><a href="#"> <?= includeSVG("edit-profile") ?>企業公開ページ作成</a></li>
-            <li><a href="#"> <?= includeSVG("database") ?>登録情報</a></li>
-            <li><a href="#"> <?= includeSVG('key-skeleton') ?>パスワード変更</a></li>
-            <li><a href="#"> <?= includeSVG('question-circle') ?>よくあるご質問</a></li>
-            <li><a href="#"> <?= includeSVG('person-carry') ?>ログアウト</a></li>
+            <li><a href="?page=page1"> <?= includeSVG("user-cog") ?> 学生検索</a></li>
+            <li><a href="?page=page9"> <?= includeSVG("email") ?> メッセージ <i class="counter"> 14 </i></a></li>
+            <li><a href="?page=page5"> <?= includeSVG("table") ?>スケジュール管理</a></li>
+            <li><a href="?page=page7"> <?= includeSVG("edit-profile") ?>企業公開ページ作成</a></li>
+            <li><a href="?page=page8"> <?= includeSVG("database") ?>登録情報</a></li>
+            <li><a href="?page=page4"> <?= includeSVG('key-skeleton') ?>パスワード変更</a></li>
+            <li><a href="?page=page3"> <?= includeSVG('question-circle') ?>よくあるご質問</a></li>
+            <li><a href="?page=page2"> <?= includeSVG('person-carry') ?>ログアウト</a></li>
         </ul>
     </nav>
 </aside>
