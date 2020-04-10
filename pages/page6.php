7c95f6d1e396b01242b278e43b233016bfaab4ca
Ngo Huu Phong <ngohuuphong.147@gmail.com>
2020-04-10 13:19:52 +0900

    set width page, left size

---
+++ pages/page6.php
@@ -0,0 +1,41 @@
+<div class="page6">
+    <section>
+        <div class="cards_container">
+            <div class="card_item">
+                <div class="header">
+                    <div class="icon_wrap">
+
+                    </div>
+                </div>
+                <div class="content">
+                    <img/>
+                    <p>AAAAAAAAAAAAAAAAAAA</p>
+                    <p>BBBBBBBBBBBBBBBBBBB</p>
+                </div>
+            </div>
+            <div class="card_item">
+                <div class="header">
+                    <div class="icon_wrap">
+                        
+                    </div>
+                </div>
+                <div class="content">
+                   
+                </div>
+            </div>
+        </div>
+    </section>
+    <section>
+        <div class="lists_container">
+            <div class="header">
+                
+            </div>
+            <div class="content">
+                <div class="item_container">
+                    
+                </div>
+            </div>
+        </div>
+    </section>
+
+</div>
