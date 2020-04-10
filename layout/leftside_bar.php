928caededbf0b22045dc7d5ad6496eced7d9b094
Ngo Huu Phong <ngohuuphong.147@gmail.com>
2020-04-10 16:15:41 +0900

    update menu

--- layout/leftside_bar.php
+++ layout/leftside_bar.php
@@ -2,56 +2,56 @@
     <nav class="left-nav">
         <ul id="nav">
             <li class="current">
-                <a href="#welcome"> 
+                <a href="?page=page10"> 
                     <?= includeSVG("arrow-circle-up") ?>
                     <span>トップ</span>
                 </a>
             </li>
             <li>
-                <a href="#"> 
+                <a href="?page=page1"> 
                     <?= includeSVG("user-cog") ?> 
                     <span>学生検索</span>
                 </a>
             </li>
             <li>
-                <a href="#"> 
+                <a href="?page=page9"> 
                     <?= includeSVG("email") ?> 
                     <span>メッセージ</span> 
                     <span class="counter"> 14 </span>
                 </a>
             </li>
             <li>
-                <a href="#"> 
+                <a href="?page=page5"> 
                     <?= includeSVG("table") ?>
                     <span>スケジュール管理</span>
                 </a>
             </li>
             <li>
-                <a href="#"> 
+                <a href="?page=page7"> 
                     <?= includeSVG("edit-profile") ?>
                     <span>企業公開ページ作成</span>
                 </a>
             </li>
             <li>
-                <a href="#"> 
+                <a href="?page=page8"> 
                     <?= includeSVG("database") ?>
                     <span>登録情報</span>
                 </a>
             </li>
             <li>
-                <a href="#"> 
+                <a href="?page=page4"> 
                     <?= includeSVG('key-skeleton') ?>
                     <span>パスワード変更</span>
                 </a>
             </li>
             <li>
-                <a href="#"> 
+                <a href="?page=page3"> 
                     <?= includeSVG('question-circle') ?>
                     <span>よくあるご質問</span>
                 </a>
             </li>
             <li>
-                <a href="#"> 
+                <a href="?page=page2"> 
                     <?= includeSVG('person-carry') ?>
                     <span>ログアウト</span>
                 </a>
