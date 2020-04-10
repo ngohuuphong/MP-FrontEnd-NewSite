faa446c5ac0f7e0e4826035dd703d7b063fbfb74
Ngo Huu Phong <ngohuuphong.147@gmail.com>
2020-04-10 09:14:52 +0900

    Update Layout

--- config/config.php
+++ config/config.php
@@ -5,10 +5,6 @@ $cfg['site']['assest_path'] = $cfg['site']['base_url'] . "assets/";
 
 $cfg['site']['pages'] = [
     'index',
-    'page1',
-    'page2',
-    'page3',
     'entryprise-student',
-    'dev_phong_page1'
 ];
 ?>
