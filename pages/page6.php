b38d9eb625d267a35053f5962142bab3ed495f68
Ngo Huu Phong <ngohuuphong.147@gmail.com>
2020-04-10 17:51:02 +0900

    Update page6

--- pages/page6.php
+++ pages/page6.php
@@ -3,24 +3,38 @@
         <div class="cards_container">
             <div class="card_item">
                 <div class="header">
-                    <div class="icon_wrap">
-
-                    </div>
+                    <img class="icon_wrap" src="<?php echo $cfg['site']['assest_path']; ?>image/icon/cardiconheader1.png" alt="">
                 </div>
                 <div class="content">
-                    <img/>
-                    <p>AAAAAAAAAAAAAAAAAAA</p>
-                    <p>BBBBBBBBBBBBBBBBBBB</p>
+                    <div>
+                        <img class="image_content img1" src="<?php echo $cfg['site']['assest_path']; ?>image/cardimage1.png" alt="">
+                        <img class="image_content img2" src="<?php echo $cfg['site']['assest_path']; ?>image/cardimage2.png" alt="">
+                    </div>
+                    <p class="p1">トラストグロース商事株式会社</p>
+                    <p class="p2">【オファーメール代行中】</p>
                 </div>
             </div>
             <div class="card_item">
                 <div class="header">
-                    <div class="icon_wrap">
-                        
-                    </div>
+                    <img class="icon_wrap" src="<?php echo $cfg['site']['assest_path']; ?>image/icon/cardiconheader2.png" alt="">
                 </div>
                 <div class="content">
-                   
+                   <div class="item">
+                        <p class="title">
+                                学生へのオファー数
+                        </p>
+                        <p class="num">
+                                35<span>件</span>
+                        </p>
+                   </div>
+                   <div class="item">
+                        <p class="title">
+                                学生からのオファー数
+                        </p>
+                        <p class="num">
+                                21<span>件</span>
+                        </p>
+                   </div>
                 </div>
             </div>
         </div>
