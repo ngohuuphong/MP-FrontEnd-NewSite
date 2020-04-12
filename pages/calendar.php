3a2049dd41cfe2324911a830e41289ea131a949a
Hùng tt <thanhhung.code@gmail.com>
2020-04-12 14:58:36 +0900

    update

--- pages/calendar.php
+++ pages/calendar.php
@@ -4,34 +4,11 @@
 <script>
 
 
-function next() {
-    currentYear = (currentMonth === 11) ? currentYear + 1 : currentYear;
-    currentMonth = (currentMonth + 1) % 12;
-    showCalendar(currentMonth, currentYear);
-}
-
-function previous() {
-    currentYear = (currentMonth === 0) ? currentYear - 1 : currentYear;
-    currentMonth = (currentMonth === 0) ? 11 : currentMonth - 1;
-    showCalendar(currentMonth, currentYear);
-}
-
-function jump() {
-    currentYear = parseInt(selectYear.value);
-    currentMonth = parseInt(selectMonth.value);
-    showCalendar(currentMonth, currentYear);
-}
-
-
-
-
-
-
-
 
 
 function Calendar() {
 
+    this.selector     = null;
     this.dateLoop     = 1;
 
     this.toDay        = new Date();
@@ -44,18 +21,22 @@ function Calendar() {
     this.selectMonth = this.toDay.getMonth();
     this.selectYear  = this.toDay.getFullYear();
 
-    this.firstDay     = (new Date(this.selectYear, this.selectMonth)).getDay()
+    this.firstDay = (new Date(this.selectYear, this.selectMonth)).getDay()
 
-    this.labelMonths = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"];
-    this.labelYear   = ' năm ';
-    this.labelMonth  = ' tháng ';
+    this.labelDays  = ["日", "月", "火", "水", "木", "金", "土"];
+    this.labelYear  = '年';
+    this.labelMonth = '月';
 
-    this.classCell       = 'cột';
-    this.classRow        = 'dòng';
-    this.classInToday    = 'today';
-    this.classWrapperRow = 'wrapper-row';
-    this.classHeader     = 'table-calendar-header';
-    this.classTable      = 'table-calendar'
+    this.classCell       = 'calendar-cell';
+    this.classRow        = 'calendar-row';
+    this.classInToday    = 'calendar-today';
+    this.classInSunday   = 'calendar-sunday';
+    this.classInSatday   = 'calendar-satday';
+    this.classWrapperRow = 'calendar-wrapper-row';
+    this.classHeader     = 'calendar-table-header';
+    this.classTable      = 'calendar-table'
+    this.classFooter     = 'calendar-footer';
+    this.classLabelHeader= 'calendar-list-header';
 
     this.setCurrentDay = function( _day ){
 
@@ -71,6 +52,7 @@ function Calendar() {
         this.selectDate  = this.indexDate.getDate();
         this.selectMonth = this.indexDate.getMonth();
         this.selectYear  = this.indexDate.getFullYear();
+        this.firstDay    = (new Date(this.selectYear, this.selectMonth)).getDay()
     }
     this.setSelectMonth = function( _month ){
 
@@ -78,6 +60,7 @@ function Calendar() {
         this.selectDate  = this.indexDate.getDate();
         this.selectMonth = this.indexDate.getMonth();
         this.selectYear  = this.indexDate.getFullYear();
+        this.firstDay    = (new Date(this.selectYear, this.selectMonth)).getDay()
     }
     this.setSelectYear = function( _year ){
 
@@ -85,6 +68,11 @@ function Calendar() {
         this.selectDate  = this.indexDate.getDate();
         this.selectMonth = this.indexDate.getMonth();
         this.selectYear  = this.indexDate.getFullYear();
+        this.firstDay    = (new Date(this.selectYear, this.selectMonth)).getDay()
+    }
+    this.setElementDraw = function(_selector){
+
+        this.selector = _selector;
     }
 
     // check how many days in a month code
@@ -93,7 +81,7 @@ function Calendar() {
         return 32 - new Date(iYear, iMonth, 32).getDate();
     }
 
-    this.draw = function(selector){
+    this.draw = function(){
 
         var table           = document.createElement("div");
             table.className = this.classTable;
@@ -103,21 +91,97 @@ function Calendar() {
 
         var tableBody = this.createCells();
         table.appendChild(tableBody);
+
+        var footer = this.createFooter();
+        table.appendChild(footer);
         
-        if( selector ){
-            
-            selector.appendChild(table);
+        if( this.selector ){
+            this.selector.innerHTML = '';
+            this.selector.appendChild(table);
         }
     }
 
     this.createHeader = function(){
 
-        header       = document.createElement("div");
+        header           = document.createElement("div");
         header.className = this.classHeader;
-        header.innerHTML =  this.selectYear + this.labelYear + 
-                            this.labelMonths[this.selectMonth] + this.labelMonth
-                            this.selectDate
+        header.innerHTML = this.selectYear + this.labelYear +
+                            ( this.selectMonth + 1 ) + this.labelMonth
+                            + this.selectDate
+
+
+        titleHeading           = document.createElement("ul");
+        titleHeading.className = this.classLabelHeader;
+
+        for(var head = 0; head < this.labelDays.length; head++){
+
+            var thead  = document.createElement("li");
+            thead.innerHTML =  this.labelDays[head];
+            
+            titleHeading.appendChild(thead);
+        }
+
+        header.appendChild(titleHeading);
         return header;
+    }
+
+    this.createFooter = function(){
+
+        var footer       = document.createElement("div");
+        footer.appendChild(this.drawFooter());
+
+        return footer;
+    }
+    this.drawFooter = function(){
+
+        var footer           = document.createElement("ul");
+            footer.className = this.classFooter;
+
+        var PREV = "<<<";
+        var NEXT = ">>>";
+
+        var TAG_PREV           = document.createElement("li");
+            TAG_PREV.innerHTML = PREV;
+
+        footer.appendChild(TAG_PREV);
+
+        var YEAR_PREV = this.selectYear - 1;
+        var TAG_YEAR_PREV           = document.createElement("li");
+            TAG_YEAR_PREV.innerHTML = YEAR_PREV;
+
+        footer.appendChild(TAG_YEAR_PREV);
+
+        for(var month = 0; month < 12; month ++ ){
+
+            var link  = document.createElement("li");
+            link.innerHTML =  ( month + 1 )+ this.labelMonth;
+            if( month == this.selectMonth ){
+                link.classList.add('active-footer');
+            }
+            (function(_month, instance){
+
+                link.addEventListener("click", function() {
+
+                    instance.setSelectMonth(_month);
+                    instance.draw();
+                }, false);
+            })(month , this);
+
+            footer.appendChild(link);
+        }
+
+        var YEAR_NEXT = this.selectYear + 1;
+        var TAG_YEAR_NEXT           = document.createElement("li");
+            TAG_YEAR_NEXT.innerHTML = YEAR_NEXT;
+
+        footer.appendChild(TAG_YEAR_NEXT);
+
+        var TAG_NEXT           = document.createElement("li");
+            TAG_NEXT.innerHTML = NEXT;
+
+        footer.appendChild(TAG_NEXT);
+
+        return footer;
     }
 
     this.createCell = function(i, j){
@@ -128,7 +192,7 @@ function Calendar() {
 
         if (i === 0 && j < this.firstDay) {
             
-            cell.innerHTML = '';
+            cell.innerHTML = '&nbsp;';
         }
         else if (this.dateLoop > this.daysInMonth(this.selectMonth, this.selectYear)) {
 
@@ -137,16 +201,49 @@ function Calendar() {
 
             cell.innerHTML = this.dateLoop;
             // color today's date
-            if (    this.dateLoop === this.currentDate && 
-                    this.selectYear  === this.currentYear &&
-                    this.selectMonth === this.currentMonth
-            ) {
+            if(this.checkToday()){
                 cell.classList.add(this.classInToday);
+            }
+            // color sunday
+            if(this.checkSunDay()){
+                cell.classList.add(this.classInSunday);
+            }
+            // color satDay
+            if(this.checkSatDay()){
+                cell.classList.add(this.classInSatday);
             }
 
             this.dateLoop++;
         }
         return cell;
+    }
+    this.checkToday = function(){
+
+        if (    this.dateLoop === this.currentDate && 
+                this.selectYear  === this.currentYear &&
+                this.selectMonth === this.currentMonth
+        ) {
+            return true;
+        }
+        return false;
+    }
+    this.checkSunDay = function(){
+        var dayNumber = this.dateLoop;
+        var theDateLoop = new Date( this.selectYear, this.selectMonth, dayNumber );
+        
+        if( theDateLoop.getDay() == 0 ){
+            return true;
+        }
+        return false;
+    }
+    this.checkSatDay = function(){
+        var dayNumber = this.dateLoop;
+        var theDateLoop = new Date( this.selectYear, this.selectMonth, dayNumber );
+        
+        if( theDateLoop.getDay() == 6 ){
+            return true;
+        }
+        return false;
     }
     this.createCells = function(){
 
@@ -181,6 +278,7 @@ function Calendar() {
 var selector = document.getElementById('draw-calendar')
 
 var insatnceCalendar = new Calendar();
-insatnceCalendar.draw(selector);
+insatnceCalendar.setElementDraw(selector);
+insatnceCalendar.draw();
 
 </script>
