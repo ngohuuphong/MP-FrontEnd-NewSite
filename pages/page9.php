e3d1aee6fd62ebeac77c06ea2bbc93675fc2b15d
Ngo Huu Phong <ngohuuphong.147@gmail.com>
2020-04-13 13:44:21 +0900

    update

--- pages/page9.php
+++ pages/page9.php
@@ -7,7 +7,75 @@
                     <span>学生検索</span>
                 </div>
                 <div class="content">
-                   
+                   <span class="span1">フリーワード</span>
+                   <div>
+                       <input class="textbox" type="text" name="" id="" placeholder="例）学校名、資格"/>
+                   </div>
+                   <span class="span2">詳細項目</span>
+                   <div>
+                       <div class="rows">
+                           <div class="item">
+                                <span>現住所</span>
+                                <select class="selectbox" id="cars">
+                                    <option value="volvo"></option>
+                                    <option value="saab">Saab</option>
+                                    <option value="mercedes">Mercedes</option>
+                                    <option value="audi">Audi</option>
+                                </select>
+                           </div>
+                           <div class="item">
+                                <span>希望勤務地</span>
+                                <select class="selectbox" id="cars">
+                                    <option value="volvo"></option>
+                                    <option value="saab">Saab</option>
+                                    <option value="mercedes">Mercedes</option>
+                                    <option value="audi">Audi</option>
+                                </select>
+                           </div>
+                           <div class="item">
+                                <span>学歴</span>
+                                <select class="selectbox" id="cars">
+                                    <option value="volvo"></option>
+                                    <option value="saab">Saab</option>
+                                    <option value="mercedes">Mercedes</option>
+                                    <option value="audi">Audi</option>
+                                </select>
+                           </div>
+                         
+                       </div>
+                       <div class="rows row2">
+                           <div class="item">
+                                <span>卒業年度</span>
+                                <select class="selectbox" id="cars">
+                                    <option value="volvo"></option>
+                                    <option value="saab">Saab</option>
+                                    <option value="mercedes">Mercedes</option>
+                                    <option value="audi">Audi</option>
+                                </select>
+                           </div>
+                           <div class="item">
+                                <span>希望業種</span>
+                                <select class="selectbox" id="cars">
+                                    <option value="volvo"></option>
+                                    <option value="saab">Saab</option>
+                                    <option value="mercedes">Mercedes</option>
+                                    <option value="audi">Audi</option>
+                                </select>
+                           </div>
+                           <div class="item">
+                                <span>希望職種</span>
+                                <select class="selectbox" id="cars">
+                                    <option value="volvo"></option>
+                                    <option value="saab">Saab</option>
+                                    <option value="mercedes">Mercedes</option>
+                                    <option value="audi">Audi</option>
+                                </select>
+                           </div>
+                         
+                       </div>
+                      
+                   </div>
+                   <!-- <span class="span3">キーワード（✓チェックを入れてください）</span> -->
                 </div>
             </div>
         </div>
