131d946390df36c9688d6b1a7acf77e3a22cbef5
Ngo Huu Phong <ngohuuphong.147@gmail.com>
2020-04-14 09:59:13 +0900

    update create page 10

---
+++ pages/page10.php
@@ -0,0 +1,36 @@
+<div class="page10">
+    <section>
+        <div class="cards_container">
+            <div class="card_item">
+                <div class="header">
+                    <span>オファーメッセージ</span>
+                </div>
+                <div class="content">
+                   <div class="item">
+                        <p class="p1">オファーメッセージリスト</p>
+                        <div class="img_div">
+                            <a href="">
+                                <img class="" src="<?php echo $cfg['site']['assest_path']; ?>image/icon/icon_card_mess.png" alt="">
+                                <img class="" src="<?php echo $cfg['site']['assest_path']; ?>image/icon/icon_card_mess_hover.png" alt="">
+                            </a>
+                            <div class="notice"><span>10</span></div>
+                        </div>
+                        <p class="p2">メッセージの送受信はこちら</p>
+                   </div>
+                   <div class="item">
+                        <p class="p1">アプローチリスト</p>
+                        <div class="img_div">
+                            <a href="">
+                                <img class="" src="<?php echo $cfg['site']['assest_path']; ?>image/icon/icon_card_like.png" alt="">
+                                <img class="" src="<?php echo $cfg['site']['assest_path']; ?>image/icon/icon_card_like_hover.png" alt="">
+                            </a>
+                            <div class="notice"><span>10</span></div>
+                        </div>
+                        <p class="p2">学生からのアプローチはこちら</p>
+                   </div>
+                  
+                </div>
+            </div>
+        </div>
+    </section>
+</div>
