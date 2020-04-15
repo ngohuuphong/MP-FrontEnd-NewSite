46159253f5611da8f87f125c9aa3323c536e2593
Trương Thanh hùng <thuong_thanh_hung@managerment-partners.co.jp>
2020-04-15 15:32:10 +0900

    update

--- pages/page7.php
+++ pages/page7.php
@@ -1,4 +1,5 @@
-<div class="page-create-company">
+
+        <div class="page-create-company">
             <div class="button-head">
                 <a href="javascript:void(0)" class="btn-create-view position-relative">
                     <span class="icon-btn">
@@ -22,54 +23,54 @@
                     <div class="input-group">
                         <div class="float-left larger-input">
                             <label for="">企業名*</label>
-                            <input type="text" placeholder="トラストグロース商事" />
+                            <input type="text" placeholder="トラストグロース商事">
                         </div>
                     </div>
 
                     <div class="input-group">
                         <div class="float-left normal-input">
                             <label for="">担当者氏名*</label>
-                            <input type="text" placeholder="トラスト" />
+                            <input type="text" placeholder="トラスト">
                         </div>
                         <div class="float-left normal-input">
                             <label for="">担当者氏名（フリガナ）*</label>
-                            <input type="text" placeholder="太郎" />
+                            <input type="text" placeholder="太郎">
                         </div>
                     </div>
 
                     <div class="input-group">
                         <div class="float-left normal-input">
                             <label for="">電話番号（ハイフン無し）*</label>
-                            <input type="text" placeholder="0333231234" />
+                            <input type="text" placeholder="0333231234">
                         </div>
                         <div class="float-left larg-input email">
                             <label for="">メールアドレス（マイページID）*</label>
-                            <input type="text" placeholder="krc3457hc@trust-growth.co.jp" />
+                            <input type="text" placeholder="krc3457hc@trust-growth.co.jp">
                         </div>
                     </div>
 
                     <div class="input-group">
                         <div class="float-left normal-input">
                             <label for="">郵便番号（ハイフン無し）*</label>
-                            <input type="text" />
+                            <input type="text">
                         </div>
                     </div>
 
                     <div class="input-group">
                         <div class="float-left normal-input">
                             <label for="">都道府県 *</label>
-                            <input type="text"  />
+                            <input type="text">
                         </div>
                         <div class="float-left normal-input">
                             <label for="">市区町村 *</label>
-                            <input type="text"  />
+                            <input type="text">
                         </div>
                     </div>
 
                     <div class="input-group">
                         <div class="float-left largest-input">
                             <label for="">番地以下 *</label>
-                            <input type="text"  />
+                            <input type="text">
                         </div>
                     </div>
 
@@ -87,20 +88,20 @@
                                 <option value="選択">選択</option>
                             </select>
                         </div>
-                        <span  class="float-left text-select ">月</span>
-                        <span  class="float-left text-select ">設立</span>
+                        <span class="float-left text-select ">月</span>
+                        <span class="float-left text-select ">設立</span>
                     </div>
 
                     <div class="input-group">
                         
                         <div class="float-left small-input">
                             <label for="">資本金</label>
-                            <input type="text"  />
+                            <input type="text">
                         </div>
                         <span class="float-left text-input ">円</span>
                         <div class="float-left small-input fix-small-input-margin">
                             <label for="">従業員数</label>
-                            <input type="text"  />
+                            <input type="text">
                         </div>
                         <span class="float-left text-input ">名</span>
                     </div>
@@ -108,7 +109,7 @@
                     <div class="input-group">
                         <div class="float-left normal-input">
                             <label for="">売上高</label>
-                            <input type="text" />
+                            <input type="text">
                         </div>
                         <span class="float-left text-input ">円</span>
                     </div>
@@ -117,11 +118,23 @@
                     <div class="input-group">
                         <div class="float-left largest-input">
                             <label for="">企業URL</label>
-                            <input type="text"  />
+                            <input type="text">
                         </div>
                     </div>
 
                     <div class="divider"><span>企業カテゴリ</span></div>
+                    <div class="wrapper-select-custom">
+                        <div class="block-select2">
+                            <select multiple class="js-render-select2-multi">
+                                <option value="自動車・電気・機械" selected>自動車・電気・機械</option>
+                                <option value="その他（メーカー）" selected >その他（メーカー）</option>
+                                <option value="通信（IT）" selected >通信（IT）</option>
+                                <option value="test">test</option>
+                            </select>
+                        </div>
+                        <a href="javascript:void(0)" class="btn-add-select2">+追加する</a>
+                    </div>
                 </div>
             </div>
-        </div>
+        </div>    
+
