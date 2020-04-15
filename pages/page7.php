ddee451e210ef10fdfcbbb779005e12f1681455c
Trương Thanh hùng <thuong_thanh_hung@managerment-partners.co.jp>
2020-04-15 18:01:50 +0900

    j

--- pages/page7.php
+++ pages/page7.php
@@ -136,5 +136,107 @@
                     </div>
                 </div>
             </div>
-        </div>    
+
+
+
+
+            <div class="cart ">
+                <div class="block-intro">
+                    <div class="cart-title">
+                        会社紹介
+                    </div>
+                    <div class="content">
+                        <textarea name="" rows="12">若手社員が実力を発揮！ 医療・福祉分野に強みを持つ総合人材サービス企業
+
+トラストグロースは2008年8月、医療・福祉分野専門の人材紹介サービス企業としてスタート
+しました。以来10年にわたり、人材不足に悩む同分野のお客さま企業を力強くサポート。同時
+に丁寧なケアと手厚いフォローにより、求職者さまからの厚い信頼も得ています。
+この分野での豊富な実績こそ、当社の原点。それを土台に、近年はIT・クリエイティブ・製造・
+事務・販売・新卒支援などへも事業分野を拡大しています。
+
+当社の主要業務は人材営業。入社後は、ビジネスマナーや基礎的な業務知識を学び、その後は先
+輩社員に同行して営業のノウハウを学んでいきます。もちろん、基本プロセスの遵守は求められ
+ますが、完全にマニュアル通りではなく、各人の個性やアイデアを生かす形で指導。「その人にし
+かできない営業」を目標に育成・指導を実践し、社員一人ひとりのスキルアップを図ります。</textarea>
+
+                        <div class="img-intro">
+                            <img src="assets/image/img-default.png" alt=""/>
+                        </div>
+                        <a href="javascript:void(0)" class="btn-create-upload position-relative">
+                            <span class="icon-btn">
+                                <img src="assets/image/icon/icon-button-upload.png" alt="">
+                            </span>
+                            PDFをダウンロード
+                        </a>
+                        <div class="devider-block"></div>
+                    </div>
+                </div>
+                <div class="block-intro">
+                    <div class="cart-title">
+                        会社データ
+                    </div>
+                    <div class="content">
+                        <textarea name="" rows="12">2008年に医療・福祉に特化した派遣会社としてスタートしました。
+現在はIT・製造・オフィスワークなど、広く分野を広げて事業拡大中です。
+東京本社のほか、北海道（札幌）・東北（仙台）・中部（名古屋）・関西（大阪）・九州（博多）に
+支社を持ち、地域医療・福祉、地域雇用に貢献しています。
+
+◆20代が中心に活躍中
+フレッシュな社員で構成される職場には、
+若手に対するチャンスが溢れています！
+2017年度より新卒採用を開始し、1期生として21名が入社しました。
+20代の所長・課長も在籍。
+年次や社歴で評価しません！意欲次第でどんどん成長できます！</textarea>
+
+                        <div class="img-intro">
+                            <img src="assets/image/img-default.png" alt=""/>
+                        </div>
+                        <a href="javascript:void(0)" class="btn-create-upload position-relative">
+                            <span class="icon-btn">
+                                <img src="assets/image/icon/icon-button-upload.png" alt="">
+                            </span>
+                            PDFをダウンロード
+                        </a>
+                        <div class="devider-block"></div>
+                    </div>
+                </div>
+                <div class="block-intro">
+                    <div class="cart-title">
+                        事業内容
+                    </div>
+                    <div class="content">
+                        <textarea name="" rows="12">高齢者（児童）福祉および医療に関わる人材派遣紹介事業
+医師、看護師、薬剤師、保育士への職業紹介および再就職支援事業
+ITエンジニアの人材派遣紹介事業
+ITアウトソージング
+システム開発事業
+新卒支援事業
+飲食、販売、物流、オフィスワーク系等の総合人材派遣紹介事業 
+
+一般労働者派遣許可番号　（般）13-304089
+有料職業紹介許可番号　13-ユ-303680
+雇用給付金取扱職業紹介事業者
+再就職給付金取扱職業紹介事業者</textarea>
+
+                        <div class="img-intro">
+                            <img src="assets/image/img-default.png" alt=""/>
+                        </div>
+                        <a href="javascript:void(0)" class="btn-create-upload position-relative">
+                            <span class="icon-btn">
+                                <img src="assets/image/icon/icon-button-upload.png" alt="">
+                            </span>
+                            PDFをダウンロード
+                        </a>
+                    </div>
+                </div>
+
+                
+            </div>
+        </div>   
+        
+        
+
+
+
+        
 
